6e4c8d45179fae3ab5a6f975fa7237b93b313665
luizinhohg <loizhg96@gmail.com>
2022-05-26 22:39:45 -0300

    up

--- Trab05/Program.cs
+++ Trab05/Program.cs
@@ -9,42 +9,67 @@ namespace Ex04._02
             int n1 = 50;
             int n2 = 60;
             int n3 = 30;
+            int contadorN1 = n1;
+            int contadorN2 = n2;
+            int contadorN3 = n3;
+            int[] diviseiveisN1 = new int[contadorN1];
+            int[] diviseiveisN2 = new int[contadorN2];
+            int[] diviseiveisN3 = new int[contadorN3];
+
+
+            int contador = 1;
+            
+
+           
+
 
 
             int divisor = 2;
 
             do
             {
+                Console.WriteLine($"Divisor {divisor}");
+                
                 if (n1 % divisor == 0)
                 {
-                    Console.WriteLine($"Numero {n1}");
-                    Console.WriteLine($"Divisor {divisor}");
+                    Console.WriteLine($"Numero {n1}");                    
                     n1 = (n1 / divisor);
                     Console.WriteLine($"{n1}");
+                    diviseiveisN1[contador] = divisor;
+                    
+
+
                 }
-                else if (n2 % divisor == 0)
+                if (n2 % divisor == 0)
                 {
-                    Console.WriteLine($"Numero {n2}");
-                    Console.WriteLine($"Divisor {divisor}");
+                    Console.WriteLine($"Numero {n2}");                    
                     n2 = (n2 / divisor);
                     Console.WriteLine($"{n2}");
+                    diviseiveisN2[contador] = divisor;
+                    
                 }
-                else if (n3 % divisor == 0)
+                if (n3 % divisor == 0)
                 {
-                    Console.WriteLine($"Numero {n3}");
-                    Console.WriteLine($"Divisor {divisor}");
+                    Console.WriteLine($"Numero {n3}");                    
                     n3 = (n3 / divisor);
                     Console.WriteLine($"{n3}");
-                }
+                    diviseiveisN3[contador] = divisor;
+                    
+                }                
                 else
-                {
-                    divisor++;
+                {                    
+                    contador++;
+                    divisor++;                  
                 }
+
+                Console.WriteLine($"CONTADOR {contador}");
+
+
             } while (n1 != 1 && n2 != 1 && n3 != 1);
             divisor = 2;
 
 
-
+            
             
 
 
